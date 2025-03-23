5fdd59c6f303cb1918d999c87be7da9c84481236
marcos <dev-marcosvieira@gmail.com>
2025-03-23 00:29:37 -0300

    second commit

---
+++ src/database.ts
@@ -0,0 +1,17 @@
+import {knex as setupKnex, Knex } from 'knex'
+
+export const config = {
+    client: 'sqlite',
+    connection: {
+        filename: './database/app.db',
+    },
+    
+    useNullAsDefault: true,
+
+    migrations:{
+        extension: 'ts',
+        directory: './database/migration'
+    }
+}
+
+export const knex = setupKnex(config)
