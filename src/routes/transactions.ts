34f4319d98c6f99e83c8da9a81b624189e663b2d
marcos <dev-marcosvieira@gmail.com>
2025-03-24 01:23:47 -0300

    create more router the table transaction

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -5,6 +5,33 @@ import { z } from "zod";
 
 export async function transactionsRoutes(app: FastifyInstance) {
 
+    app.get('/', async(request, replay) => {
+
+        const transactions = await knex('transactions').select()
+
+        return replay.send({transactions})
+    })
+
+    app.get('/:id', async(request, replay) => {
+
+        const getTransactionParams = z.object({
+            id: z.string().uuid()
+        })
+
+        const { id } = getTransactionParams.parse(request.params)
+
+        const transaction = await knex('transactions').select().where({id}).first()
+
+        return replay.send({transaction})
+    })
+
+    app.get('/summary', async(resquest, replay) => {
+
+        const summary = await knex('transactions').sum('amount', { as: 'amount' }).first()
+
+        return replay.send({summary})
+    })
+
     app.post('/', async(request, replay) =>{
 
         const createTransactionBodySchema = z.object({
@@ -22,9 +49,5 @@ export async function transactionsRoutes(app: FastifyInstance) {
         })
 
         return replay.status(201).send()
-      
-    })
-
-
-    
+    }) 
 }
