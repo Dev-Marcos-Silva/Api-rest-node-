a989372636c629bcd703cf59c64f7ff1219c4ecd
Dev-Marcos-Silva <dev.marcosvieira@gmail.com>
2025-03-25 23:56:32 -0300

    cookie users

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -46,10 +46,24 @@ export async function transactionsRoutes(app: FastifyInstance) {
 
         const { title, amount, type } = createTransactionBodySchema.parse(request.body)
 
+        let sessionId = request.cookies.sessionId
+
+        if(!sessionId){
+
+            sessionId = randomUUID()
+
+            replay.setCookie('sessionId', sessionId,{
+                path: '/',
+                maxAge: 60 * 60 * 24 * 7 // 7 day
+            }) 
+
+        }
+
         await knex('transactions').insert({
             id: randomUUID(),
             title,
-            amount: type === 'credit'? amount : amount * -1
+            amount: type === 'credit'? amount : amount * -1,
+            session_id: sessionId
         })
 
         return replay.status(201).send()
